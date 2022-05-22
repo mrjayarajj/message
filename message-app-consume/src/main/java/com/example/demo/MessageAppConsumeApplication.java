485e183d845f957cddebd7eec392b07ac55535f9
Jayaraj Jaganathan <mrjayarajj@gmail.com>
2022-05-22 16:36:03 -0400

    initial commit

--- message-app-consume/src/main/java/com/example/demo/MessageAppConsumeApplication.java
+++ message-app-consume/src/main/java/com/example/demo/MessageAppConsumeApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 
 @Slf4j
 @SpringBootApplication
@@ -12,11 +16,11 @@ public class MessageAppConsumeApplication {
 		SpringApplication.run(MessageAppConsumeApplication.class, args);
 	}
 
-	/*
+
 	@KafkaListener(topics = "MESSAGE-TOPIC", groupId = "consumer-groupid")
 	public void consume(@Payload KafkaMessage message, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String messageKey) {
 		log.info("KafkaMessage :"+message);
 		log.info("messageKey :"+messageKey);
-	}*/
+	}
 
 }
